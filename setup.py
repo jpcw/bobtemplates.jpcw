f47674211a9af2d9c884213e766f0d9f209b19cc
Jean-Philippe Camguilhem <jp.camguilhem@gmail.com>
2013-10-29 07:28:27 +0100

    update Python version classifiers and comment jinja2.6 force

--- setup.py
+++ setup.py
@@ -33,6 +33,13 @@ setup(name='bobtemplates.jpcw',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "License :: OSI Approved :: BSD",
         ],
       keywords='bobtemplates',
